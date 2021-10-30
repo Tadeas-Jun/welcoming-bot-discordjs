47f5e0663ab94b0cd44d6d9ed9b0941e00482233
Tadeas Salvatore Jun <darezar@eledris.com>
2021-10-30 13:32:51 +0200

    Updated the bot to Discord.js v13.3.0

--- bot.js
+++ bot.js
@@ -1,14 +1,14 @@
-const Discord = require('discord.js');
+const { Client, Intents } = require('discord.js');
 
-const client = new Discord.Client();
+const client = new Client({intents: [Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS]});
 
-const WELCOME_CHANNEL_ID = '123456789012345678';
+const WELCOME_CHANNEL_ID = 'YOUR WELCOMING CHANNEL ID';
 
-console.log('Starting.');
+console.log('Starting bot, please give me a second.');
 
 client.on('ready', () => {
 
-    console.log('I am ready!');
+    console.log('I am ready to welcome people!');
 
 });
 
@@ -28,15 +28,23 @@ function WelcomeNewMember(member) {
         `Would you like to choose a role, ${member}?`
     ];
 
-    const channel = member.guild.channels.cache.find(ch => ch.id === WELCOME_CHANNEL_ID);
+    const channel = client.channels.fetch(WELCOME_CHANNEL_ID)
+    .then(channel => {
 
-    if (!channel) return;
+        let welcomeMessage = WELCOME_MESSAGES[Math.floor(Math.random() * WELCOME_MESSAGES.length)];
 
-    let welcomeMessage = WELCOME_MESSAGES[Math.floor(Math.random() * WELCOME_MESSAGES.length)];
+        // Sending the welcome message 1 second after the member joins, to make sure they get properly tagged.
+        setTimeout(function() { 
+            
+            console.log("Welcoming a new member.");
 
-    console.log("Welcoming a new member.");
+            channel.send(welcomeMessage);
+            
+        }, 1000);
 
-    channel.send(welcomeMessage);
+    })
+
+    .catch(console.error);
 
 }
 
